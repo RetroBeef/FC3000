a2d9ea5bc26b3f2e46fcb02d53bf74a765e8daa8
c3pp0 <cepnet@gmail.com>
2024-05-20 03:49:31 +0200

    setting mtu size for proper notifcations; added methods to set time and timer

--- FC3000.cpp
+++ FC3000.cpp
@@ -1,15 +1,17 @@
 #include "FC3000.h"
 
 namespace {
-  static BLEUUID serviceUUID("83677baa-3eb8-4866-b6b6-96e5ed5cc48d");
-  static BLEUUID writeUUID("f5d2b3fe-e6b5-49b5-aa5f-a00bb4156d1d");
-  static BLEUUID readUUID("6f588463-f8f1-44f8-bdae-a1272a1b0f6e");
+static BLEUUID serviceUUID("83677baa-3eb8-4866-b6b6-96e5ed5cc48d");
+static BLEUUID writeUUID("f5d2b3fe-e6b5-49b5-aa5f-a00bb4156d1d");
+static BLEUUID readUUID("6f588463-f8f1-44f8-bdae-a1272a1b0f6e");
 }
 
 int8_t FC3000::connectToDevice() {
   if (!bleClient->connect(BLEAddress(deviceMac.c_str()))) {
     return -1;
   }
+
+  bleClient->setMTU(517);
 
   fc3000Service = bleClient->getService(serviceUUID);
   if (fc3000Service == nullptr) {
@@ -29,27 +31,38 @@ int8_t FC3000::connectToDevice() {
   if (readCharacteristic->canNotify()) {
     readCharacteristic->registerForNotify(notificationCallback);
     return 0;
-  }else {
+  } else {
     return -5;
   }
 }
 
-void FC3000::dimm(const uint8_t percent){
-    if(percent<10){
-        off();
-        return;
-    }
-    String perc = percent > 100 ? "100" : String((int)percent);
-    String command = "2|0|" + deviceId + "|" + perc;
-    writeCharacteristic->writeValue((uint8_t*)command.c_str(), command.length(), true);
+void FC3000::dimm(const uint8_t percent) {
+  if (percent < 10) {
+    off();
+    return;
+  }
+  String perc = percent > 100 ? "100" : String((int)percent);
+  String command = "2|0|" + deviceId + "|" + perc;
+  writeCharacteristic->writeValue((uint8_t*)command.c_str(), command.length(), true);
 }
 
-void FC3000::on(){
-    String command = "1|0|" + deviceId + "|1";
-    writeCharacteristic->writeValue((uint8_t*)command.c_str(), command.length(), true);
+void FC3000::on() {
+  String command = "1|0|" + deviceId + "|1";
+  writeCharacteristic->writeValue((uint8_t*)command.c_str(), command.length(), true);
 }
 
-void FC3000::off(){
-    String command = "1|0|" + deviceId + "|0";
-    writeCharacteristic->writeValue((uint8_t*)command.c_str(), command.length(), true);
+void FC3000::off() {
+  String command = "1|0|" + deviceId + "|0";
+  writeCharacteristic->writeValue((uint8_t*)command.c_str(), command.length(), true);
+}
+
+
+void FC3000::setTime(const uint32_t& unixtime) {
+  String command =  "6|1|20631|ALL|" + String(unixtime);//todo get 20631 from name
+  writeCharacteristic->writeValue((uint8_t*)command.c_str(), command.length(), true);
+}
+
+void FC3000::setTimer(const String & startTimeHHMM, const uint16_t durationMinutes) {
+  String command = "60|0|" + deviceId + "|1|" + startTimeHHMM + "," + String((int)durationMinutes) + "|0";
+  writeCharacteristic->writeValue((uint8_t*)command.c_str(), command.length(), true);
 }
--- examples/FC3000/FC3000.ino
+++ examples/FC3000/FC3000.ino
@@ -12,51 +12,59 @@ void setup() {
   BLEDevice::init("");
   bleClient = BLEDevice::createClient();
 
-  growlight = new FC3000(bleClient, "08:d1:f9:65:29:ee", [](BLERemoteCharacteristic *pBLERemoteCharacteristic, uint8_t *pData, size_t length, bool isNotify){
+  growlight = new FC3000(bleClient, "08:d1:f9:65:29:ee", [](BLERemoteCharacteristic * pBLERemoteCharacteristic, uint8_t *pData, size_t length, bool isNotify) {
     char data[length + 1];
     data[length] = '\0';
     memcpy(data, pData, length);
     char* cmdStart = strstr(data, "cmd");
-    if (cmdStart != NULL) {//no status if timer disabled?
+    if (cmdStart != NULL) {
       cmdStart += 4;
       int state, dimmPercentage, dc1, dc2;
       unsigned long localTime;
       unsigned long dc3;
       sscanf(cmdStart, "%d|%d|%d|%d|%ld|%lu", &state, &dimmPercentage, &dc1, &dc2, &localTime, &dc3);
       Serial.printf("state(%u), dimmPercentage(%u), localUnixTime(%u)\r\n", state, dimmPercentage, localTime);
-    }else{
+    } else {
       Serial.println(data);
     }
   });
-  if(growlight->connectToDevice() != 0){
+  if (growlight->connectToDevice() != 0) {
     Serial.println("could not connect to device");
-  }else{
+  } else {
     Serial.println("connected");
+    while (Serial.available() > 0) {//clear input
+      Serial.read();
+    }
   }
 }
 
 void loop() {
-  if (Serial.available()) {
+  if (Serial.available() && growlight->isConnected()) {
     String s = Serial.readString();
-    if(s.length()){
-        if(s.indexOf("on")>-1){
-            Serial.println("turning on grow light");
-            growlight->on();
-        }else if(s.indexOf("off")>-1){
-            Serial.println("turning off grow light");
-            growlight->off();
-        }else{
-            uint8_t percent = (uint8_t)s.toInt();
-            Serial.printf("dimming grow light to %u%%\r\n", percent);
-            growlight->dimm(percent);
-        }
+    if (s.length()) {
+      if (s.indexOf("on") > -1) {
+        Serial.println("turning on grow light");
+        growlight->on();
+      } else if (s.indexOf("off") > -1) {
+        Serial.println("turning off grow light");
+        growlight->off();
+      } else if (s.indexOf("test") > -1) {
+        Serial.println("setting time to 1716068211");
+        growlight->setTime(1716068211);
+        Serial.println("setting timer to 21:37 with 2 min duration");
+        growlight->setTimer("21:37", 2);
+      } else {
+        uint8_t percent = (uint8_t)s.toInt();
+        Serial.printf("dimming grow light to %u%%\r\n", percent);
+        growlight->dimm(percent);
+      }
     }
   }
   if (!growlight->isConnected()) {
-    if(growlight->connectToDevice() != 0){
+    if (growlight->connectToDevice() != 0) {
       Serial.println("could not connect to device");
-    }else{
-    Serial.println("connected");
-  }
+    } else {
+      Serial.println("connected");
+    }
   }
 }
